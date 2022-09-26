c26548f7d993fbdbeaa4802c68a8d47d92c2c75e
Karnaukhova Iuliia <julialobankova123@gmail.com>
2022-09-26 22:58:17 +0300

    s1t2

---
+++ Sem/sem1/T2.py
@@ -0,0 +1,13 @@
+# Напишите программу, которая на вход принимает 5 чисел (можно списком)
+# и находит максимальное из них.
+# Примеры:
+# 1. 1, 4, 8, 7, 5 -> 8
+# 78, 55, 36, 90, 2 -> 90
+
+numbers = list(map(int, input().split()))
+max = 0
+for i in range(len(numbers)):
+    if numbers[i] > max:
+        max = numbers[i]
+print('{}, -> {}'.format(numbers, max))
+        
