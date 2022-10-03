5b126692c218627d82dd5113b3fe6791712855c9
Karnaukhova Iuliia <julialobankova123@gmail.com>
2022-10-03 23:00:10 +0300

    dz2-1

--- DZ/dz2/t1.py
+++ DZ/dz2/t1.py
@@ -4,10 +4,9 @@
 #Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
 n = int(input('Введите число '))
-def dictionary(n):
-    list = []
-    i = 1
-    while (i <= n):
-        list = ['{}: {}'.format(i, 3*i+1)]
-        return list
-print(list)
+dictionary = {}
+for i in range(1, n+1):
+    b = 3*i + 1
+    dictionary[i] = b
+print(dictionary, end=" ")
+
