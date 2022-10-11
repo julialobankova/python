3375295a4435c83ab3cfe76e83b96ba3e587b6c5
Karnaukhova Iuliia <julialobankova123@gmail.com>
2022-10-11 22:25:27 +0300

    dz41

--- DZ/dz4/t1.py
+++ DZ/dz4/t1.py
@@ -2,10 +2,7 @@
 # Пример:
 # - при d = 0.001, π = 3.141    10^{-1} ≤ d ≤10^{-10}
 
-
-def accuracy(num, acc):
-    number = Decimal(f"{num}")
-    return number.quantize(Decimal(f"{acc}"))
-
-
-print(accuracy(float(input("Введите число: ")), float(input("Введите точность округления в формате 0.0001: "))))
+import math
+d = input('Введите точность: ')
+lend = len(d) - 2
+print(f"pi = {math.pi:.{lend}f}")
