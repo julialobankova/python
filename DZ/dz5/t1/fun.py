3f57b296572a6c822ed5246c948e94c27fdd7fae
Karnaukhova Iuliia <julialobankova123@gmail.com>
2022-10-17 21:39:41 +0300

    dz51л

---
+++ DZ/dz5/t1/fun.py
@@ -0,0 +1,3 @@
+def info (player1, player2, candy):
+    print('1й игрок {} конфет, 2й игрок {} конфет, {} осталось конфет'.format(player1, player2, candy))
+
---
+++ DZ/dz5/t1/t1bot.py
@@ -0,0 +1,49 @@
+
+#Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
+# a) Добавьте игру против бота
+# b) Подумайте как наделить бота ""интеллектом""
+
+from random import randint
+from fun import info
+candies = 2021
+candy_player1 = 0
+candy_player2 = 0
+player = randint(1, 2)
+print('Начнем игру, первый ход определяется жеребьёвкой, за раз можно взять не более 28 конфет, погнали')
+
+if player == 1:
+    p1 = int(input('Первый игрок: '))
+    candies = candies - p1
+    candy_player1 += p1 
+    player = 2
+    info(candy_player1, candy_player2,candies)
+else:
+    p2 = randint(1, 29)
+    candies = candies - p2
+    candy_player2 += p2 
+    player = 1
+    print('Ходит бот')
+    info(candy_player1, candy_player2,candies)
+
+while candies >= 0:
+    if player == 1:
+        p1 = int(input('Первый игрок: '))
+        candies = candies - p1
+        candy_player1 += p1 
+        info(candy_player1, candy_player2,candies)
+        player = 2
+        if candies <= 0:
+            print('Вы победили')
+            break
+    else:
+        p2 = randint(1, 29)
+        candies = candies - p2
+        candy_player2 += p2
+        print('Ходит бот')
+        info(candy_player1, candy_player2,candies)
+        player = 1 
+        if candies <= 0:
+            print('Вы победили!')
+            break
+
+
